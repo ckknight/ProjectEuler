1c1656bbef2a2ecc131992cb9ff950cd0a13d2cc
ckknight <ckknight@gmail.com>
2010-05-02 22:45:24 -0500

    Problem 7

---
+++ Ckknight.ProjectEuler/Problems/Problem007.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ckknight.ProjectEuler.Collections;
+
+namespace Ckknight.ProjectEuler.Problems
+{
+    [Problem(7,
+        @"By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+        What is the 10001st prime number?")]
+    public class Problem007 : BaseProblem
+    {
+        public override object CalculateResult()
+        {
+            return PrimeGenerator.GetPrimeAtIndex(10001 - 1);
+        }
+    }
+}
--- Ckknight.ProjectEuler/Program.cs
+++ Ckknight.ProjectEuler/Program.cs
@@ -11,7 +11,7 @@ namespace Ckknight.ProjectEuler
     {
         static void Main(string[] args)
         {
-            RunProblem(6);
+            RunProblem(7);
         }
 
         public static void RunProblem(int number)
