8a0cf745d836894429346449338b85cc768fcc87
ckknight <ckknight@gmail.com>
2010-05-05 18:09:37 -0500

    Problem 29

--- Ckknight.ProjectEuler/Program.cs
+++ Ckknight.ProjectEuler/Program.cs
@@ -12,7 +12,7 @@ namespace Ckknight.ProjectEuler
     {
         static void Main(string[] args)
         {
-            RunProblem(28);
+            RunProblem(29);
         }
 
         public static void RunProblem(int number)
