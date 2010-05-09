ce86ff0b049aa8cfb585981a4e201ca6cc687178
ckknight <ckknight@gmail.com>
2010-05-09 11:20:03 -0500

    Problem 35

---
+++ Ckknight.ProjectEuler/Problems/Problem035.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ckknight.ProjectEuler.Collections;
+
+namespace Ckknight.ProjectEuler.Problems
+{
+    [Problem(35,
+        @"The number, 197, is called a circular prime because all rotations of
+        the digits: 197, 971, and 719, are themselves prime.
+
+        There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31,
+        37, 71, 73, 79, and 97.
+
+        How many circular primes are there below one million?")]
+    public class Problem035 : BaseProblem
+    {
+        public override object CalculateResult()
+        {
+            return new PrimeGenerator()
+                .TakeWhile(n => n < 1000000)
+                .Select(n => MathUtilities.ToDigitList(n))
+                .Where(n => new Range(1, n.Count)
+                    .All(i => PrimeGenerator.IsPrime(
+                        MathUtilities.FromDigitList(
+                            n.Skip(i).Concat(n.Take(i))))))
+                .Count();
+        }
+    }
+}
